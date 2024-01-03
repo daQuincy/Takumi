24a0bc4b43bb2a76084f5c7dea31e7d060bf460b
Quincy <quincy_lim@protonmail.com>
2024-01-03 16:01:11 +0800

    minor update

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+from sklearn.metrics import confusion_matrix
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 import pytz
 
@@ -24,3 +29,58 @@ def read_bm_data(file_path):
     df_wti['day'] = df_wti['datetime'].dt.strftime('%A')
 
     return df_wti
+
+def plot_confusion_matrix(y_true, y_pred, title='', normalize=False):
+    if normalize:
+        cm = confusion_matrix(y_true, y_pred, normalize='pred')
+        sns.heatmap(cm, annot=True, fmt='.2f', cmap='Blues')
+    else:
+        cm = confusion_matrix(y_true, y_pred)
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+
+    plt.title(title)
+    plt.ylabel('True')
+    plt.xlabel('Predicted')
+    plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_confusion_matrix(y_true, y_pred, class_names, title='Confusion Matrix', cmap=plt.cm.Blues, normalize=False):
+    """
+    Plots a confusion matrix with clear visualization.
+
+    Args:
+        cm (np.ndarray): The confusion matrix to plot.
+        class_names (list): A list of class names for the matrix.
+        title (str, optional): Title for the plot. Defaults to 'Confusion Matrix'.
+        cmap (matplotlib colormap, optional): Colormap for the matrix. Defaults to plt.cm.Blues.
+    """
+
+    if normalize:
+        cm = confusion_matrix(y_true, y_pred, normalize='pred')
+    else:
+        cm = confusion_matrix(y_true, y_pred)
+
+    plt.figure(figsize=(4, 4))  # Set appropriate figure size
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45, ha='right')
+    plt.yticks(tick_marks, class_names)
+
+    # Normalize for better visualization
+    # cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in np.ndindex(cm.shape):
+        plt.text(j, i, f"{cm[i, j]:.2f}", ha="center", va="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
